6a67202afc02a4b2cba373bafc8045e5b546f07a
aakaash123 <20190113@iiitdmj.ac.in>
2024-05-08 23:24:05 +0530

    add some css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    Navigate
-} from "react-router-dom";
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/navbar';
-
-import './App.css';
 import Footer from "./components/footer";
 import Home from "./components/Home";
+import Background from './components/Background';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-            <Footer />
-        </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                {/*<Background/>*/}
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                </Routes>
+                <Footer/>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Background.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Background.css';  // Assuming the CSS above is saved in this file
+
+const Background = () => {
+    return <div className="background"></div>;
+};
+
+export default Background;
